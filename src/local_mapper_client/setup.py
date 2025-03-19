a67eb2dda63bc4bea6a2aa77dfa025ffe23bb3de
Noel Csomay-Shanklin <noelcs@caltech.edu>
2025-03-18 19:27:34 -0700

    fixed rviz config path

--- src/local_mapper_client/setup.py
+++ src/local_mapper_client/setup.py
@@ -13,6 +13,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),  # Install launch files
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),  # Install rviz files
     ],
     install_requires=['setuptools'],
     zip_safe=True,
